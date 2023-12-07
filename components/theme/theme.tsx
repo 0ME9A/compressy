ddf2b498265e56caef8062ba2a61bdaf683cb8cf
kishore881 <kishorereddykancherla@gmail.com>
2023-12-07 10:31:04 +0530

    update theme in redux store on site load

--- components/theme/theme.tsx
+++ components/theme/theme.tsx
@@ -1,18 +1,22 @@
 "use client";
+import { setTheme } from "@/RTK/slices/themeSlice";
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function Theme() {
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
+    const dark = localStorage.theme === "dark" || window.matchMedia("(prefers-color-scheme: dark)").matches;
+    
+    if (dark) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
-  }, []);
+    
+    dispatch(setTheme(dark))
+  }, [dispatch]);
 
   return <></>;
 }
